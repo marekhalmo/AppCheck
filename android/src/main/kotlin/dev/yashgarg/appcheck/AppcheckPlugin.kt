ef1e275aaa95d6d51d55272a524579b491d93328
marekhalmo <halmomarek@gmail.com>
2024-12-20 11:13:08 +0100

    Update AppcheckPlugin.kt

--- android/src/main/kotlin/dev/yashgarg/appcheck/AppcheckPlugin.kt
+++ android/src/main/kotlin/dev/yashgarg/appcheck/AppcheckPlugin.kt
@@ -85,13 +85,20 @@ class AppcheckPlugin : FlutterPlugin, MethodCallHandler {
     private fun convertPackageInfoToJson(info: PackageInfo): Map<String, Any> {
         val app: MutableMap<String, Any> = HashMap()
 
-        app["app_name"] = 
-            info.applicationInfo != null ? info.applicationInfo.loadLabel(context.packageManager).toString() : "N/A"
+        if(info.applicationInfo != null) {
+            app["app_name"] = 
+                info.applicationInfo.loadLabel(context.packageManager).toString()
+            app["icon"] = DrawableUtil.drawableToByteArray(info.applicationInfo.loadIcon(context.packageManager))
+            app["system_app"] = (info.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
+        } else {
+            app["app_name"] = "N/A"
+            app["system_app"] = false
+        }
+        
         app["package_name"] = info.packageName
-        app["version_name"] = info.versionName
-        app["icon"] = DrawableUtil.drawableToByteArray(info.applicationInfo?.loadIcon(context.packageManager))
+        app["version_name"] = info.versionName        
         app["version_code"] = getVersionCode(info)
-        app["system_app"] = (info.applicationInfo?.flags and ApplicationInfo.FLAG_SYSTEM) != 0
+        
         return app
     }
 
